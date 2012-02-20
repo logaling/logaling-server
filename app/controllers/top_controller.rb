626e0436ba90d1aa7d8b8e0032df1b033badd0b8
Yoji SHIDARA <dara@shidara.net>
2012-02-20 16:47:05 +0900

    add link to projects

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @projects = GithubProject.all
   end
 end
--- app/models/github_project.rb
+++ app/models/github_project.rb
@@ -1,2 +1,5 @@
 class GithubProject < ActiveRecord::Base
+  def full_name
+    "%s/%s" % [owner, name]
+  end
 end
