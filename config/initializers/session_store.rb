12da2ccf854141972c07aa785551678f968c6d4e
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-07-02 11:55:13 +0900

    Update generated files by rails command
    Exec `rake rails:update`.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-LogalingServer::Application.config.session_store :cookie_store, key: '_logaling-server_session'
+LogalingServer::Application.config.session_store :cookie_store, key: '_logaling_server_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
