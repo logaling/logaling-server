e0c70386c5776aa9b823344df5cfd5c6725e932a
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-09-19 20:13:37 +0900

    Add set_user_glossary to before filter for TermsController

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,16 +1,16 @@
 #coding: utf-8
 class TermsController < ApplicationController
+  before_filter :set_user_glossary
+
   def new
     @term = Term.new
   end
 
   def create
     @term = Term.new(params[:term])
+    @user_glossary.add!(@term)
 
-    user_glossary = UserGlossary.find(params[:glossary_id])
-    user_glossary.add!(@term)
-
-    redirect_to user_glossary_path(current_user, user_glossary), notice: 'Term was successfully added.'
+    redirect_to user_glossary_path(current_user, @user_glossary), notice: 'Term was successfully added.'
   rescue => e
     @term.errors.add(:source_term, e) if @term.errors.empty?
     render action: "new"
@@ -23,5 +23,12 @@ class TermsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_user_glossary
+    @user_glossary = UserGlossary.find(params[:glossary_id])
+  rescue ActiveRecord::RecordNotFound
+    render :file => 'public/404.html', :status => 404, :layout => false
   end
 end
