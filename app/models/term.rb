75e749537b5a907e7aa1f503db2a5c566b965e6c
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-09-19 17:32:42 +0900

    Revert "Move Term into LogalingServer module"
    This reverts commit 664ab4e9f9bed8a3ee2a42ff8e9d785e0fb38ebc.

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,13 +1,10 @@
 #coding: utf-8
-module LogalingServer
-  class Term
-    include ActiveAttr::Model
+class Term
+  include ActiveAttr::Model
 
-    attribute :source_term, type: String, default: ''
-    attribute :target_term, type: String, default: ''
-    attribute :note, type: String, default: ''
+  attribute :source_term, type: String, default: ''
+  attribute :target_term, type: String, default: ''
+  attribute :note, type: String, default: ''
 
-    validates_presence_of :source_term, :target_term
-  end
+  validates_presence_of :source_term, :target_term
 end
-
