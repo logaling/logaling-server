68f467abe18af4845b920731be3b2e11a6db13af
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-09-19 18:44:58 +0900

    Relegate user_glossary to local variable

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -7,10 +7,10 @@ class TermsController < ApplicationController
   def create
     @term = Term.new(params[:term])
 
-    @user_glossary = UserGlossary.find(params[:glossary_id])
-    @user_glossary.add!(@term)
+    user_glossary = UserGlossary.find(params[:glossary_id])
+    user_glossary.add!(@term)
 
-    redirect_to user_glossary_path(current_user, @user_glossary), notice: 'Term was successfully added.'
+    redirect_to user_glossary_path(current_user, user_glossary), notice: 'Term was successfully added.'
   rescue => e
     @term.errors.add(:source_term, e) if @term.errors.empty?
     render action: "new"
