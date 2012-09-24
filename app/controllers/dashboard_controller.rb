a413b3fe79f3c594d4631a1ea3458fa0ace2bfea
SUZUKI Miho <adzuki34@gmail.com>
2012-09-24 11:09:00 +0900

    Set empty UserConfig when user does not have UserConfig

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticate!
 
   def show
-    @user_config = UserConfig.find_by_user_id(current_user.id)
+    @user_config = UserConfig.find_by_user_id(current_user.id) || UserConfig.new
     @user_glossaries = UserGlossary.find_all_by_user_id(current_user.id)
   end
 end
