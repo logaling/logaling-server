029e697616d9936e3901994afd79a32c995089e8
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-09-26 08:36:06 +0900

    Remove 'self'

--- app/decorators/user_config_decorator.rb
+++ app/decorators/user_config_decorator.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 module UserConfigDecorator
   def mark_for(user_glossary)
-    self.same?(user_glossary) ? "★" : "☆"
+    same?(user_glossary) ? "★" : "☆"
   end
 
   def submit_path_for(user)
-    if self.new_record?
+    if new_record?
       user_configs_path(user)
     else
       user_config_path(user, self)
