2bae1fa2a08585a94dd4396dcf9f4a33ef0f86ba
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-12-05 11:43:47 +0900

    Use ImportedProject for showing information

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,12 @@ class PagesController < ApplicationController
   def explore
     @github_projects = GithubProject.all
     @user_glossaries = UserGlossary.all
+    @imported_projects = imported_projects
+  end
+
+  private
+  #TODO: Logaling::Repository#imported_projects を用意して、そちらを使う
+  def imported_projects
+    LogalingServer.repository.projects.select {|project| project.is_a? Logaling::ImportedProject }
   end
 end
