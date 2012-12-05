139580840c0b97e3786710acc3266faef69a31ee
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-12-05 12:06:58 +0900

    Use Logaling::Repository#imported_projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,12 +6,6 @@ class PagesController < ApplicationController
   def explore
     @github_projects = GithubProject.all
     @user_glossaries = UserGlossary.all
-    @imported_projects = imported_projects
-  end
-
-  private
-  #TODO: Logaling::Repository#imported_projects を用意して、そちらを使う
-  def imported_projects
-    LogalingServer.repository.projects.select {|project| project.is_a? Logaling::ImportedProject }
+    @imported_projects = LogalingServer.repository.imported_projects
   end
 end
